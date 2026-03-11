220e3f98a28edfdc86dc96e7b24e02908578887d
Kott Q <constantinekuncel@gmail.com>
2026-03-11 17:54:36 +0100

    solved

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,102 @@
-from app.split_integer import split_integer
+import pytest
+from app import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+# Test 1
+@pytest.mark.parametrize(
+    "val, num, result",
+    [
+        (2, 2, 2),
+        (3, 3, 3),
+        (4, 2, 4),
+        (6, 3, 6),
+        (17, 7, 17),
+        (31, 11, 31),
+        (100, 9, 100),
+    ],
+)
+def test_sum_of_the_parts_should_be_equal_to_value(
+    val: int, num: int, result: int
+) -> None:
+    assert (
+        sum(split_integer.split_integer(val, num)) == result
+    ), f"Sum of the parts should be equal to {result}, but it isn't"
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+# Test 2
+@pytest.mark.parametrize(
+    "v, n, result",
+    [
+        (2, 2, [1, 1]),
+        (3, 3, [1, 1, 1]),
+        (4, 2, [2, 2]),
+        (6, 3, [2, 2, 2]),
+        (8, 4, [2, 2, 2, 2]),
+        (9, 3, [3, 3, 3]),
+        (10, 5, [2, 2, 2, 2, 2]),
+        (12, 6, [2, 2, 2, 2, 2, 2]),
+    ],
+)
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+    v: int, n: int, result: list
+) -> None:
+    assert (
+        split_integer.split_integer(v, n) == result
+    ), f"Function should split {v} into {n} equal parts, but it doesn't"
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+# Test 3
+@pytest.mark.parametrize(
+    "v, n, result",
+    [
+        (5, 1, [5]),
+        (11, 1, [11]),
+        (17, 1, [17]),
+        (31, 1, [31]),
+        (100, 1, [100]),
+    ],
+)
+def test_should_return_part_equals_to_value_when_split_into_one_part(
+    v: int, n: int, result: list
+) -> None:
+    assert (
+        split_integer.split_integer(v, n) == result
+    ), f"Function should return {result} when {v} is split into one part, but it doesn't"
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+# Test 4
+@pytest.mark.parametrize(
+    "v, n, result",
+    [
+        (5, 2, [2, 3]),
+        (11, 3, [3, 4, 4]),
+        (17, 4, [4, 4, 4, 5]),
+        (31, 5, [6, 6, 6, 6, 7]),
+        (100, 9, [11, 11, 11, 11, 11, 11, 11, 11, 12]),
+    ],
+)
+def test_parts_should_be_sorted_when_they_are_not_equal(
+    v: int, n: int, result: list
+) -> None:
+    assert (
+        split_integer.split_integer(v, n) == result
+    ), f"Function should return {result} when {v} is split into {n} parts, but it doesn't"
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+# Test 5
+@pytest.mark.parametrize(
+    "v, n, result",
+    [
+        (1, 3, [0, 0, 1]),
+        (2, 5, [0, 0, 0, 1, 1]),
+        (5, 10, [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]),
+        (7, 12, [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]),
+        (10, 15, [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+    ],
+)
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+    v: int, n: int, result: list
+) -> None:
+    assert (
+        split_integer.split_integer(v, n) == result
+    ), f"Function should return {result} when {v} is split into {n} parts, but it doesn't"
